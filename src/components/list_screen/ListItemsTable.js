b34d4f688d564262594d7f0d41dbc13176eb12b0
ZP4 <jasonhuzheming9@gmail.com>
2019-10-13 17:35:14 -0400

    Fixed sorting listItems for Firefox

--- src/components/list_screen/ListItemsTable.js
+++ src/components/list_screen/ListItemsTable.js
@@ -25,14 +25,16 @@ export class ListItemsTable extends Component {
             currentStatusSortingCriteria: sortingCriteria.SORT_STATUS_INCREASING,
             list: [].concat(this.props.todoList.items)
         };
-
+        this.sortItem = this.sortItem.bind(this);
+        this.compare = this.compare.bind(this);
     }
 
-    sortItem(category) {
+    sortItem = (category) => {
         let array = this.state.list;
         if (category === sortCategory.TASK) {
+            //this.compare(a.description, b.description, this.state.currentTaskSortingCriteria)
             array = array.sort((a, b) => this.compare(a.description, b.description, this.state.currentTaskSortingCriteria));
-
+            console.log(array);
             if (this.state.currentTaskSortingCriteria === sortingCriteria.SORT_TASK_INCREASING) {
                 this.setState({currentTaskSortingCriteria: sortingCriteria.SORT_TASK_DECREASING})
             }
@@ -43,12 +45,14 @@ export class ListItemsTable extends Component {
 
         else if (category === sortCategory.DATE) {
             array = array.sort((a, b) => this.compare(a.due_date, b.due_date, this.state.currentDateSortingCriteria));
+            console.log(array);
             this.state.currentDateSortingCriteria === sortingCriteria.SORT_DATE_INCREASING
                 ? this.setState({currentDateSortingCriteria: sortingCriteria.SORT_DATE_DECREASING})
                 : this.setState({currentDateSortingCriteria: sortingCriteria.SORT_DATE_INCREASING})
         }
         else if (category === sortCategory.STATUS) {
             array = array.sort((a, b) => this.compare(a.completed, b.completed, this.state.currentStatusSortingCriteria));
+            console.log(array);
             this.state.currentStatusSortingCriteria === sortingCriteria.SORT_STATUS_INCREASING
                 ? this.setState({currentStatusSortingCriteria: sortingCriteria.SORT_STATUS_DECREASING})
                 : this.setState({currentStatusSortingCriteria: sortingCriteria.SORT_STATUS_INCREASING})
@@ -56,7 +60,7 @@ export class ListItemsTable extends Component {
         this.setState({list: array});
     }
 
-    compare(item1, item2, criteria) {
+    compare = (item1, item2, criteria) => {
         if (criteria === sortingCriteria.SORT_TASK_DECREASING ||
             criteria === sortingCriteria.SORT_DATE_DECREASING ||
             criteria === sortingCriteria.SORT_STATUS_DECREASING
@@ -69,27 +73,18 @@ export class ListItemsTable extends Component {
         if (criteria === sortingCriteria.SORT_STATUS_DECREASING ||
             criteria === sortingCriteria.SORT_STATUS_INCREASING
         ) {
-            if (item1 > item2) {
-                return -1
-            }
-            else if (item2 < item1) {
-                return 1
-            }
-            else {
-                return 0
-            }
+            return item1-item2;
         }
 
+        else if (criteria === sortingCriteria.SORT_DATE_INCREASING ||
+                 criteria === sortingCriteria.SORT_DATE_DECREASING
+        ) {
+            item1 = new Date(item1);
+            item2 = new Date(item2);
+            return item1-item2
+        }
         else {
-            if (item1.toLowerCase() > item2.toLowerCase()) {
-                return -1
-            }
-            else if (item2.toLowerCase() < item1.toLowerCase()) {
-                return 1
-            }
-            else {
-                return 0
-            }
+            return item1.localeCompare(item2)
         }
 
     }
@@ -98,9 +93,9 @@ export class ListItemsTable extends Component {
         return (
             <div className= "list_item_header_card" id="list_items_container">
                 <div className="list_item_header_card"> </div>
-                <div className="list_item_task_header" onClick={() => (this.sortItem(sortCategory.TASK))}>Task</div>
-                <div className="list_item_due_date_header"onClick={() => (this.sortItem(sortCategory.DATE))}>Due Date</div>
-                <div className="list_item_status_header"onClick={() => (this.sortItem(sortCategory.STATUS))}>Status</div>
+                <div className="list_item_task_header" onClick={() => (this.sortItem(sortCategory.TASK))} >Task</div>
+                <div className="list_item_due_date_header"onClick={(event) => (this.sortItem(sortCategory.DATE))}>Due Date</div>
+                <div className="list_item_status_header"onClick={(event) => (this.sortItem(sortCategory.STATUS))}>Status</div>
                 {
                     this.state.list.map((todoItem)=>(
                         <ListItemCard
