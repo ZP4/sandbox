cd826491ca64b8d13eb41025e73358148eb65421
ZP4 <jasonhuzheming9@gmail.com>
2019-10-14 17:51:18 -0400

    updated css

--- src/components/item_screen/ItemScreen.js
+++ src/components/item_screen/ItemScreen.js
@@ -5,7 +5,24 @@ export class ItemScreen extends Component {
     render() {
         return (
             <div>
-                THIS HAS NOTHING SO FAR!
+                <strong >Item</strong>
+                <br/><br/>
+                <strong>Description:</strong>
+                <input id="edit_description_textfield" type="text"/>
+                <br/><br/>
+                <strong>Assigned To:</strong>
+                <input id="edit_assigned_to_textfield" type="text"/>
+                <br/><br/>
+                <strong>Due Date:</strong>
+                <input id="edit_due_date_datefield" type="date"/>
+                <br/><br/>
+                <strong>Completed:</strong>
+                <input id="edit_completed_checkbox" type="checkbox" />
+                <br/><br/>
+                <div>
+                    <button id="edit_submit_button">Submit</button>
+                    <button id="edit_cancel_button">Cancel</button>
+                </div>
             </div>
         )
     }
